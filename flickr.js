3dffd84954cafe1d9222de6dbc748252a5efc53a
Dan Phiffer <dan@phiffer.org>
2014-03-26 21:47:18 -0400

    Added required params

--- flickr.js
+++ flickr.js
@@ -18,22 +18,125 @@ function Flickr(init) {
   
   var self = this;
   
-  function createMethod(method, handler) {
-    var settings = jQuery.extend(true, {}, ajaxSettings);
-    settings.data.method = method;
-    return function(args, callback) {
-      jQuery.extend(settings.data, args);
+  function getCallback(callback) {
+    if (typeof callback === 'function') {
+      return callback;
+    } else if (typeof console === 'object' &&
+               typeof console.log === 'function') {
+      return function(response) {
+        console.log(response);
+      };
+    }
+  }
+  
+  function responseHandler(response, callback, params) {
+    callback = getCallback(callback);
+    if (typeof response === 'object') {
+      if (response.photos &&
+          response.photos.photo) {
+        for (var i = 0; i < response.photos.photo.length; i++) {
+          response.photos.photo[i] = new FlickrPhoto(self, response.photos.photo[i]);
+        }
+      } else if (response.photoset &&
+                 response.photoset.photo) {
+        for (var i = 0; i < response.photoset.photo.length; i++) {
+          response.photoset.photo[i] = new FlickrPhoto(self, response.photoset.photo[i]);
+        }
+      }
+      callback.apply(self, [response, params]);
+    } else {
+      callback.apply(self, [{
+        stat: 'fail',
+        code: -1,
+        message: 'No response.'
+      }, params]);
+    }
+  }
+  
+  function getParams(params, required) {
+    if (typeof params === 'undefined') {
+      return {};
+    } else if (typeof params !== 'object' &&
+        required.length === 1) {
+      var key = required[0];
+      var value = params;
+      var params = {};
+      params[key] = value;
+      return params;
+    } else {
+      return params;
+    }
+  }
+  
+  function validateParams(method, params, required) {
+    var missingParams = [], key;
+    for (var i = 0; i < required.length; i++) {
+      param = required[i];
+      if (!params[param]) {
+        missingParams.push(param);
+      }
+    }
+    if (missingParams.length > 0 &&
+        typeof console === 'object' &&
+        typeof console.warn === 'function') {
+      console.warn('Flickr API method ' + method +
+                   ' requires: ' + missingParams.join(', '));
+    }
+  }
+  
+  function getUsage(method, required) {
+    if (required.length === 0) {
+      return 'Usage: ' + method + '(callback);';
+    }
+    var usage = 'Usage: ' + method + '({\n';
+    var param;
+    for (var i = 0; i < required.length; i++) {
+      param = required[i];
+      usage += '         ' + param + ': [' + param + ']';
+      if (i < required.length - 1) {
+        usage += ',';
+      }
+      usage += '\n';
+    }
+    usage += '       }, callback);';
+    if (required.length === 1) {
+      usage += '\n\n   Or: ' + method + '([' + param + '], callback);';
+    }
+    return usage;
+  }
+  
+  function createMethod(method, required) {
+    var methodWrapper = function(params, callback) {
+      var settings = jQuery.extend(true, {}, ajaxSettings);
+      settings.data.method = method;
+      if (typeof params === 'function' &&
+          required.length === 0) {
+        callback = params;
+        params = {};
+      } else {
+        params = getParams(params, required);
+      }
+      validateParams(method, params, required);
+      jQuery.extend(settings.data, params);
       jQuery.ajax(settings).done(function(response) {
-        handler.apply(self, [callback, response, args]);
+        responseHandler.apply(self, [response, callback, params]);
       });
     };
+    methodWrapper.help = function() {
+      if (typeof console === 'object' &&
+          typeof console.info === 'function') {
+        return console.info(getUsage(method, required));
+      }
+      return getUsage(method, required);
+    };
+    return methodWrapper;
   }
   
   function setup(context, baseMethod, methods) {
     var method;
     for (var key in methods) {
       method = baseMethod + '.' + key;
-      if (typeof methods[key] === 'function') {
+      if (typeof methods[key].length == 'number') {
         context[key] = createMethod(method, methods[key]);
       } else if (typeof methods[key] === 'object') {
         context[key] = {};
@@ -42,184 +145,142 @@ function Flickr(init) {
     }
   }
   
-  function getCallback(callback) {
-    if (typeof callback === 'function') {
-      return callback;
-    } else if (typeof console === 'object' &&
-               typeof console.log === 'function') {
-      return console.log;
-    }
-  }
-  
-  function genericHandler(callback, response, args) {
-    callback = getCallback(callback);
-    if (typeof response === 'object') {
-      callback.apply(self, [response, args]);
-    } else {
-      callback.apply(self, [{
-        stat: 'fail',
-        code: -1,
-        message: 'No response.'
-      }, args]);
-    }
-  }
-  
-  function photosHandler(callback, response, args) {
-    if (typeof response === 'object' &&
-        response.photos &&
-        response.photos.photo) {
-      for (var i = 0; i < response.photos.photo.length; i++) {
-        response.photos.photo[i] = new FlickrPhoto(self, response.photos.photo[i]);
-      }
-      callback = getCallback(callback);
-      callback.apply(self, [response, args]);
-    } else if (typeof response === 'object' &&
-               response.photoset &&
-               response.photoset.photo) {
-      for (var i = 0; i < response.photoset.photo.length; i++) {
-        response.photoset.photo[i] = new FlickrPhoto(self, response.photoset.photo[i]);
-      }
-    } else {
-      genericHandler(callback, response, args);
-    }
-  }
-  
   setup(this, 'flickr', {
     cameras: {
-      getBrandModels: genericHandler,
-      getBrands: genericHandler
+      getBrandModels: ['brand'],
+      getBrands: []
     },
     collections: {
-      getTree: genericHandler
+      getTree: []
     },
     commons: {
-      getInstitutions: genericHandler
+      getInstitutions: []
     },
     contacts: {
-      getPublicList: genericHandler
+      getPublicList: ['user_id']
     },
     favorites: {
-      getContext: genericHandler,
-      getPublicList: genericHandler
+      getContext: ['photo_id', 'user_id'],
+      getPublicList: ['user_id']
     },
     galleries: {
-      getInfo: genericHandler,
-      getList: genericHandler,
-      getListForPhoto: genericHandler,
-      getPhotos: photosHandler
+      getInfo: ['gallery_id'],
+      getList: ['user_id'],
+      getListForPhoto: ['photo_id'],
+      getPhotos: []
     },
     groups: {
-      getInfo: genericHandler,
-      search: genericHandler,
+      getInfo: ['group_id'],
+      search: ['text'],
       discuss: {
         replies: {
-          getInfo: genericHandler,
-          getList: genericHandler
+          getInfo: ['topic_id', 'reply_id'],
+          getList: ['topic_id']
         },
         topics: {
-          getInfo: genericHandler,
-          getList: genericHandler
+          getInfo: ['topic_id'],
+          getList: ['group_id']
         }
       },
       pools: {
-        getContext: genericHandler,
-        getPhotos: photosHandler
+        getContext: ['photo_id', 'group_id'],
+        getPhotos: ['group_id']
       }
     },
     interestingness: {
-      getList: photosHandler
+      getList: []
     },
     machinetags: {
-      getNamespaces: genericHandler,
-      getPairs: genericHandler,
-      getPredicates: genericHandler,
-      getRecentValues: genericHandler,
-      getValues: genericHandler
+      getNamespaces: [],
+      getPairs: [],
+      getPredicates: [],
+      getRecentValues: [],
+      getValues: ['namespace', 'predicate']
     },
     panda: {
-      getList: genericHandler,
-      getPhotos: photosHandler
+      getList: [],
+      getPhotos: ['panda_name']
     },
     people: {
-      findByEmail: genericHandler,
-      findByUsername: genericHandler,
-      getInfo: genericHandler,
-      getPhotos: photosHandler,
-      getPhotosOf: photosHandler,
-      getPublicGroups: genericHandler,
-      getPublicPhotos: photosHandler
+      findByEmail: ['find_email'],
+      findByUsername: ['username'],
+      getInfo: ['user_id'],
+      getPhotos: ['user_id'],
+      getPhotosOf: ['user_id'],
+      getPublicGroups: ['user_id'],
+      getPublicPhotos: ['user_id']
     },
     photos: {
-      getAllContexts: genericHandler,
-      getContactsPublicPhotos: photosHandler,
-      getContext: genericHandler,
-      getExif: genericHandler,
-      getFavorites: genericHandler,
-      getInfo: genericHandler,
-      getRecent: photosHandler,
-      getSizes: genericHandler,
-      search: photosHandler,
+      getAllContexts: ['photo_id'],
+      getContactsPublicPhotos: ['user_id'],
+      getContext: ['photo_id'],
+      getExif: ['photo_id'],
+      getFavorites: ['photo_id'],
+      getInfo: ['photo_id'],
+      getRecent: [],
+      getSizes: ['photo_id'],
+      search: [],
       comments: {
-        getList: genericHandler
+        getList: ['photo_id']
       },
       geo: {
-        getLocation: genericHandler
+        getLocation: ['photo_id']
       },
       licenses: {
-        getInfo: genericHandler
+        getInfo: []
       },
       people: {
-        getList: genericHandler
+        getList: ['photo_id']
       }
     },
     photosets: {
-      getContext: genericHandler,
-      getInfo: genericHandler,
-      getList: genericHandler,
-      getPhotos: photosHandler,
+      getContext: ['photo_id', 'photoset_id'],
+      getInfo: ['photoset_id'],
+      getList: [],
+      getPhotos: ['photoset_id'],
       comments: {
-        getList: genericHandler
+        getList: ['photoset_id']
       }
     },
     places: {
-      find: genericHandler,
-      findByLatLon: genericHandler,
-      getChildrenWithPhotosPublic: genericHandler,
-      getInfo: genericHandler,
-      getInfoByUrl: genericHandler,
-      getPlaceTypes: genericHandler,
-      getShapeHistory: genericHandler,
-      getTopPlacesList: genericHandler,
-      placesForBoundingBox: genericHandler,
-      placesForTags: genericHandler,
-      resolvePlaceId: genericHandler,
-      resolvePlaceURL: genericHandler,
-      tagsForPlace: genericHandler
+      find: ['query'],
+      findByLatLon: ['lat', 'lon'],
+      getChildrenWithPhotosPublic: [],
+      getInfo: [],
+      getInfoByUrl: ['url'],
+      getPlaceTypes: [],
+      getShapeHistory: [],
+      getTopPlacesList: ['place_type_id'],
+      placesForBoundingBox: ['bbox'],
+      placesForTags: ['place_type_id'],
+      resolvePlaceId: ['place_id'],
+      resolvePlaceURL: ['url'],
+      tagsForPlace: []
     },
     reflection: {
-      getMethodInfo: genericHandler,
-      getMethods: genericHandler
+      getMethodInfo: ['method_name'],
+      getMethods: []
     },
     tags: {
-      getClusterPhotos: genericHandler,
-      getClusters: genericHandler,
-      getHotList: genericHandler,
-      getListPhoto: genericHandler,
-      getListUser: genericHandler,
-      getListUserPopular: genericHandler,
-      getListUserRaw: genericHandler,
-      getRelated: genericHandler
+      getClusterPhotos: ['tag', 'cluster_id'],
+      getClusters: ['tag'],
+      getHotList: [],
+      getListPhoto: ['photo_id'],
+      getListUser: ['user_id'],
+      getListUserPopular: ['user_id'],
+      getListUserRaw: ['user_id'],
+      getRelated: []
     },
     test: {
-      echo: genericHandler
+      echo: []
     },
     urls: {
-      getGroup: genericHandler,
-      getUserPhotos: genericHandler,
-      getUserProfile: genericHandler,
-      lookupGallery: genericHandler,
-      lookupGroup: genericHandler,
-      lookupUser: genericHandler
+      getGroup: [],
+      getUserPhotos: [],
+      getUserProfile: [],
+      lookupGallery: [],
+      lookupGroup: [],
+      lookupUser: []
     }
   });
 }
@@ -235,11 +296,11 @@ function FlickrPhoto(flickr, values) {
   Object.call(this);
 }
 
-FlickrPhoto.prototype.src = function(size = null, origFormat = 'jpg') {
+FlickrPhoto.prototype.src = function(size, origFormat) {
   /*
   
   http://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
-	  or
+    or
   http://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}_[mstzb].jpg
     or
   http://farm{farm-id}.staticflickr.com/{server-id}/{id}_{o-secret}_o.(jpg|gif|png)
@@ -252,6 +313,9 @@ FlickrPhoto.prototype.src = function(size = null, origFormat = 'jpg') {
   if (!size) {
     return base + '.jpg';
   } else if (size === 'o') {
+    if (!origFormat) {
+      origFormat = 'jpg';
+    }
     return base + '_o.' + origFormat;
   } else {
     return base + '_' + size + '.jpg';
